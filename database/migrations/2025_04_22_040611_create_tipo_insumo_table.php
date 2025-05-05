6dabbfe29dd514b8b7ce500a9f58b62614cbbc77
DanteUrquidez <a23311034@uthermosillo.edu.mx>
2025-05-05 14:53:13 -0700

    Capturar los proveedores

--- database/migrations/2025_04_22_040611_create_tipo_insumo_table.php
+++ database/migrations/2025_04_22_040611_create_tipo_insumo_table.php
@@ -24,6 +24,18 @@ return new class extends Migration
             $table->string('campo8')->nullable();
             $table->timestamp('created_at')->nullable();
         });
+        DB::table('tipo_insumo')->insert([
+            'nombre' => 'Telas',
+            'campo1' => 'Diseño',
+            'campo2' => 'Ancho',
+            'campo3' => 'Unidad de Medida',
+            'campo4' => 'Composición',
+            'campo5' => 'Libro',
+            'campo6' => 'Precio CO',
+            'campo7' => 'Precio RO',
+            'campo8' => 'Precio Volumen',
+            'created_at' => now(),
+        ]);
     }
 
     /**
