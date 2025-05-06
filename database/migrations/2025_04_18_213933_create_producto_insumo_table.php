8aab8882924f57f158fd21fe4e98fdfdb8cee275
DanteUrquidez <a23311034@uthermosillo.edu.mx>
2025-05-06 13:10:53 -0700

    Corrección de eliminados

---
+++ database/migrations/2025_04_18_213933_create_producto_insumo_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('producto_insumo', function (Blueprint $table) {
+            $table->foreignId('id_producto')->constrained('producto');
+            $table->foreignId('id_insumo')->constrained('insumo');
+            $table->decimal('cantidad', 10, 2)->nullable();
+            $table->primary(['id_producto', 'id_insumo']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('producto_insumo');
+    }
+};
